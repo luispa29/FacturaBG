95f4b52db0586b456be359015b4b6c05160928e4
lcevallos <luispaulcevallos@gmail.com>
2026-01-31 01:37:51 -0500

    add configuracion servicio http

---
+++ src/app/core/services/http.service.ts
@@ -0,0 +1,64 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '@environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class HttpService {
+    private http = inject(HttpClient);
+    private baseUrl = environment.apiUrl;
+
+    private getHeaders(): HttpHeaders {
+        let headers = new HttpHeaders({
+            'Content-Type': 'application/json'
+        });
+
+        const token = localStorage.getItem('auth_token');
+        if (token) {
+            headers = headers.set('Authorization', `Bearer ${token}`);
+        }
+
+        return headers;
+    }
+
+    private buildUrl(endpoint: string): string {
+        return `${this.baseUrl}${endpoint}`;
+    }
+
+    get<T>(endpoint: string, params?: any): Observable<T> {
+        let httpParams = new HttpParams();
+
+        if (params) {
+            Object.keys(params).forEach(key => {
+                if (params[key] !== null && params[key] !== undefined) {
+                    httpParams = httpParams.set(key, params[key].toString());
+                }
+            });
+        }
+
+        return this.http.get<T>(this.buildUrl(endpoint), {
+            headers: this.getHeaders(),
+            params: httpParams
+        });
+    }
+
+    post<T>(endpoint: string, body: any): Observable<T> {
+        return this.http.post<T>(this.buildUrl(endpoint), body, {
+            headers: this.getHeaders()
+        });
+    }
+
+    put<T>(endpoint: string, body: any): Observable<T> {
+        return this.http.put<T>(this.buildUrl(endpoint), body, {
+            headers: this.getHeaders()
+        });
+    }
+
+    delete<T>(endpoint: string): Observable<T> {
+        return this.http.delete<T>(this.buildUrl(endpoint), {
+            headers: this.getHeaders()
+        });
+    }
+}
